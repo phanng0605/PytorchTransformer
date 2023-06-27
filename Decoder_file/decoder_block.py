e91ed43b6cba25a626b519cc1acb19247a1eb85b
Phan Nguyen <94454067+phanng0605@users.noreply.github.com>
2023-06-28 01:26:14 +0800

    Add files via upload

---
+++ Decoder_file/decoder_block.py
@@ -0,0 +1,30 @@
+from self_attention import SelfAttention
+from transformer_block import TransformerBlock
+import torch.nn as nn
+
+
+# DECODER BLOCK
+class DecoderBlock(nn.Module):
+    def __init__(self,
+                 embed_size,
+                 heads,
+                 forward_expansion,
+                 dropout,
+                 device):
+
+        super(DecoderBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm = nn.LayerNorm(embed_size)
+        self.transformer_block = TransformerBlock(
+            embed_size, heads, dropout, forward_expansion)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, value, key, src_mask, trg_mask):
+        # masked self attention
+        attention = self.attention(x, x, x, trg_mask)
+        query = self.dropout(self.norm(attention + x))
+
+        # passing this query, value, key to a whole transformer block
+        # forward(self, value, key, query, mask):
+        out = self.transformer_block(value, key, query, src_mask)
+        return out
